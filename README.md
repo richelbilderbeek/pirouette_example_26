3ac31868908a55093ccc59bd2e9128a96454df3e
richelbilderbeek <richel@richelbilderbeek.nl>
2020-01-23 11:46:44 +0100

    Go Travis

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # pirouette_example_26
-pirouette example 26
+
+Branch   |[![Travis CI logo](pics/TravisCI.png)](https://travis-ci.org)                                                                                                 |[![AppVeyor logo](pics/AppVeyor.png)](https://appveyor.com)                                                                                               
+---------|--------------------------------------------------------------------------------------------------------------------------------------------------------------|--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+`master` |[![Build Status](https://travis-ci.org/richelbilderbeek/pirouette_example_26.svg?branch=master)](https://travis-ci.org/richelbilderbeek/pirouette_example_26) |?
+`develop`|[![Build Status](https://travis-ci.org/richelbilderbeek/pirouette_example_26.svg?branch=develop)](https://travis-ci.org/richelbilderbeek/pirouette_example_26)|?
+
+A [pirouette example](https://github.com/richelbilderbeek/pirouette_examples)
+that shows the true and twin error for a BD tree when assuming a Yule tree prior.
+
+## Results
+
+![](example_26_314/errors.png)
+
---
+++ example_26.R
@@ -0,0 +1,88 @@
+# From https://github.com/richelbilderbeek/pirouette_article/issues/57
+#
+# Write script that shows the true and twin error for Yule
+# tree and Yule tree prior
+#
+# The only difference with
+# https://github.com/richelbilderbeek/pirouette_example_3
+# is that the twin tree must also be Yule (in the other
+# example, the twin tree is BD)
+#
+suppressMessages(library(pirouette))
+suppressMessages(library(ggplot2))
+
+root_folder <- getwd()
+example_no <- 22
+rng_seed <- 314
+example_folder <- file.path(root_folder, paste0("example_", example_no, "_", rng_seed))
+dir.create(example_folder, showWarnings = FALSE, recursive = TRUE)
+setwd(example_folder)
+set.seed(rng_seed)
+testit::assert(is_beast2_installed())
+phylogeny <- create_yule_tree(n_taxa = 6, crown_age = 10)
+
+alignment_params <- create_alignment_params(
+  sim_tral_fun = get_sim_tral_with_std_nsm_fun(
+    mutation_rate = 0.1,
+    site_model = beautier::create_jc69_site_model()
+  ),
+  root_sequence = create_blocked_dna(length = 1000),
+  rng_seed = rng_seed,
+  fasta_filename = "true_alignment.fas"
+)
+
+experiment <- create_gen_experiment()
+experiment$beast2_options$input_filename <- "true_alignment_gen.xml"
+experiment$beast2_options$output_state_filename <- "true_alignment_gen.xml.state"
+experiment$inference_model$mcmc$tracelog$filename <- "true_alignment_gen.log"
+experiment$inference_model$mcmc$treelog$filename <- "true_alignment_gen.trees"
+experiment$inference_model$mcmc$screenlog$filename <- "true_alignment_gen.csv"
+experiment$errors_filename <- "true_errors_gen.csv"
+experiments <- list(experiment)
+
+# Set the RNG seed
+for (i in seq_along(experiments)) {
+  experiments[[i]]$beast2_options$rng_seed <- rng_seed
+}
+
+# Shorter on Travis
+if (is_on_travis()) {
+  for (i in seq_along(experiments)) {
+    experiments[[i]]$inference_model$mcmc$chain_length <- 3000
+    experiments[[i]]$inference_model$mcmc$store_every <- 1000
+  }
+}
+
+pir_params <- create_pir_params(
+  alignment_params = alignment_params,
+  experiments = experiments,
+  twinning_params = create_twinning_params(sim_twin_tree_fun = create_sim_yule_twin_tree_fun())
+)
+
+rm_pir_param_files(pir_params)
+
+errors <- pir_run(
+  phylogeny,
+  pir_params = pir_params
+)
+
+utils::write.csv(
+  x = errors,
+  file = file.path(example_folder, "errors.csv"),
+  row.names = FALSE
+)
+
+pir_plot(errors) +
+  ggsave(file.path(example_folder, "errors.png"), width = 7, height = 7)
+
+pir_to_pics(
+  phylogeny = phylogeny,
+  pir_params = pir_params,
+  folder = example_folder
+)
+
+pir_to_tables(
+  pir_params = pir_params,
+  folder = example_folder
+)
+
