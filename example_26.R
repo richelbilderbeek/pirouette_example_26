bc5ac8385a2dad1c4acfc8357ecb8b0091c99d8d
richelbilderbeek <richel@richelbilderbeek.nl>
2020-02-28 13:25:03 +0100

    EOD

--- example_26.R
+++ example_26.R
@@ -14,9 +14,15 @@ example_no <- 26
 rng_seed <- 314
 folder_name <- paste0("example_", example_no, "_", rng_seed)
 
+################################################################################
+# Create phylogeny
+################################################################################
 set.seed(rng_seed)
 phylogeny <- create_bd_tree(n_taxa = 6, crown_age = 10)
 
+################################################################################
+# Setup pirouette
+################################################################################
 pir_params <- create_std_pir_params(folder_name = folder_name)
 for (i in seq_along(pir_params$experiments)) {
   pir_params$experiments[[i]]$inference_model$tree_prior <- 
